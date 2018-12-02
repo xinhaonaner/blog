eac8c84b244cc1994e5aa7e77089578822c75f04
fzp <Fzhengpei@gmail.com>
2018-12-02 23:14:44 +0800

    添加支付宝红包

--- resources/views/vendor/canvas/frontend/blog/index.blade.php
+++ resources/views/vendor/canvas/frontend/blog/index.blade.php
@@ -13,6 +13,9 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-lg-2 col-lg-pull-1 col-md-pull-1" style="position: fixed">
+                <img src="https://cdn.weifengchuxing.com/app/upload/client/2018-12-02/UAfC4WDD7hTRS8CHvUzHvGmjOV2ricMSPnGscttN.png" alt="支付宝红包">
+            </div>
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 @include('canvas::frontend.blog.partials.tag')
                 @include('canvas::frontend.blog.partials.posts')
--- resources/views/vendor/canvas/frontend/shared/partials/header.blade.php
+++ resources/views/vendor/canvas/frontend/shared/partials/header.blade.php
@@ -3,6 +3,7 @@
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <h1><a href="{!! route('canvas.blog.post.index') !!}">{{ \Canvas\Models\Settings::blogTitle() }}</a></h1>
             <h3>{!! \Canvas\Models\Settings::blogSubTitle() !!}</h3>
+            {{--<h3><img src="http://cdn.weifengchuxing.com/app/upload/client/2018-12-02/UAfC4WDD7hTRS8CHvUzHvGmjOV2ricMSPnGscttN.png" alt="支付宝红包"></h3>--}}
             @if (!empty($socialHeaderIconsUser->twitter))
                 <a href="http://twitter.com/{{ $socialHeaderIconsUser->twitter }}" target="_blank" id="social"><i class="fa fa-fw fa-twitter text-muted" style="font-size: 14px"></i></a>
             @endif

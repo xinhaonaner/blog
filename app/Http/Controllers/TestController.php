3d8094d47da67026a01ff7535e895550e00dad21
fzp <Fzhengpei@gmail.com>
2019-04-19 12:49:42 +0800

    add-cmq-route

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
-    public function index() {
-        dd(2);
+    public function index(Request $request) {
+        $data = $request->input();
+        info('请求方式：' . $request->method());
+        info('请求参数:' . json_encode($data));
+        return 'success';
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get('test', 'TestController@index');
+Route::any('test', 'TestController@index');

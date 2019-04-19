af9d09d1e826cd52289ab52d0ff1bc8d8e03a38c
fzp <Fzhengpei@gmail.com>
2019-04-19 13:56:56 +0800

    edit-route

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,6 @@ class TestController extends Controller
 {
     public function index(Request $request) {
         $data  = file_get_contents("php://input");
-        info('请求方式：' . $request->method());
         info('请求参数:' . $data);
         abort(503, 'Unauthorized action.');
 //        return 'success';
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::any('test', 'TestController@index');
+Route::post('test', 'TestController@index');
